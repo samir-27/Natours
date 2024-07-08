84aee14b79d1edd212581c7aa01e675bcd896768
samir-27 <sumrasamir27@gmail.com>
2024-07-08 09:15:24 +0530

    separate controller

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan')
 const path = require('path');
-
-const PORT = 3000;
+const tourRouter = require('./Routes/TourRoutes')
+const userRouter = require('./Routes/UserRoutes')
 const app = express();
 
 //Middlewares
@@ -11,125 +10,19 @@ app.use(morgan('dev'));
 
 app.use(express.json())
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     console.log("Hello From the middleware");
     next();
 })
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     req.requestTime = new Date().toDateString();
     next();
 })
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 //Routes Handlers
 
-const getAllTours = (req, res) => {
-    console.log(req.requestTime)
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours
-        }
-    });
-}
-
-const getTour = (req, res) => {
-    console.log(req.params);
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id)
-    if (id > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'invalid ID'
-        })
-    }
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
-    });
-}
-
-const createTour = (req, res) => {
-    // console.log(req.body);
-    const newID = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newID }, req.body);
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        })
-    })
-}
-
-const updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'invalid ID'
-        })
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<UPDATED tour here...>'
-        }
-    })
-}
-
-const deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'invalid ID'
-        })
-    }
-
-    res.status(204).json({
-        status: 'success',
-        data: null
-    })
-}
-
-const getALlUsers=(req,res) =>{
-    res.status(500).json({
-        status:'error',
-        message:'this route is not yet defined'
-    })
-}
-const getUser=(req,res) =>{
-    res.status(500).json({
-        status:'error',
-        message:'this route is not yet defined'
-    })
-}
-const createUser=(req,res) =>{
-    res.status(500).json({
-        status:'error',
-        message:'this route is not yet defined'
-    })
-}
-const updateUser=(req,res) =>{
-    res.status(500).json({
-        status:'error',
-        message:'this route is not yet defined'
-    })
-}
-const deleteUser=(req,res) =>{
-    res.status(500).json({
-        status:'error',
-        message:'this route is not yet defined'
-    })
-}
 
 //ROutes
 
@@ -139,30 +32,10 @@ const deleteUser=(req,res) =>{
 // app.patch('/api/v1/tours/:id', updateTour)
 // app.delete('/api/v1/tours/:id', deleteTour)
 
-app
-    .route('/api/v1/tours')
-    .get(getAllTours)
-    .post(createTour);
 
-app
-    .route('/api/v1/tours/:id')
-    .get(getTour)
-    .patch(updateTour)
-    .delete(deleteTour);
 
-app
-    .route('/api/v1/users')
-    .get(getALlUsers)
-    .post(createUser)
-
-app 
-    .route('/api/v1/users:id')
-    .get(getUser)
-    .patch(updateUser)
-    .delete(deleteUser)
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 //Server
-
-app.listen(PORT, () => {
-    console.log(`app runing on port${PORT}`)
-})
+module.exports = app;
