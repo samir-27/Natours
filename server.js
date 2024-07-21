0a61ab8ae2afbb0b12fbb516fc704d74cc347758
samir-27 <sumrasamir27@gmail.com>
2024-07-21 17:16:44 +0530

    create simple tour model

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
-const { connection } = require('mongoose');
 
 dotenv.config({ path: './.env' });
-console.log(process.env);
+
 const PORT = 3000;
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
+// Connect to MongoDB
 mongoose
- .connect(DB,{
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindModify: false
-}).then(() =>console.log('DB connection successful'))
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful'));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A Tour Must Have a Name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  Price: {
+    type: Number,
+    require: [true, 'A Tour Must Have a Price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+// Start the server
 app.listen(PORT, () => {
-  console.log(`app running on port${PORT}`);
+  console.log(`App running on port ${PORT}`); // Added space after 'port'
 });
