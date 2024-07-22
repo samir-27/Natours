21407d6bab5f9805ea9c1bd60c1091abee99c17a
samir-27 <sumrasamir27@gmail.com>
2024-07-22 21:26:50 +0530

    updating database

--- Controllers/tourController.js
+++ Controllers/tourController.js
@@ -85,12 +85,23 @@ exports.createTour = async (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<UPDATED tour here...>'
-    }
-  });
+  try {
+    const tour = Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
