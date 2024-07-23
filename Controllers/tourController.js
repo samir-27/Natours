354836850c4aa49511acc5788ae51f42e305f298
samir-27 <sumrasamir27@gmail.com>
2024-07-23 11:40:08 +0530

    api: filtering

--- Controllers/tourController.js
+++ Controllers/tourController.js
@@ -28,8 +28,24 @@ const Tour = require('./../Models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
+    const query = Tour.find(queryObj);
+
+    // const tours = Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+
+    //EXECUTE QUERY
+    const tours = await query;
+
+    //SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -79,7 +95,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       sataus: 'fail',
-      message: 'Invalid Data Sent!'
+      message: err
     });
   }
 };
